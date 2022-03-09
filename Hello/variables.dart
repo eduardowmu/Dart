a2b298aeb850241228f87351c6718d869ba9cbea
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2022-03-08 22:32:19 -0300

    Tipos de variáveis

---
+++ Hello/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nome = "Eduardo";
+  int idade = 39;
+  double salario = 5000;
+  bool brasileiro = true;
+  print("Nome " +
+      nome +
+      ", idade " + idade.toString() +
+      ", salario " + salario.toString() +
+      ", brasileiro? " + brasileiro.toString());
+}
