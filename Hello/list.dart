273285454b7c368604cfbe08ae40058d3bc204c2
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2022-03-18 00:49:59 -0300

    Map

--- Hello/list.dart
+++ Hello/list.dart
@@ -1,4 +1,4 @@
 void main() {
   List numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Arroz", 1.56];
-  print(numeros);
+  print(numeros.length);
 }
---
+++ Hello/listMap.dart
@@ -0,0 +1,9 @@
+void main() {
+  Map usuario = {
+      'nome': 'Eduardo', 
+      'idade': 38, 
+      'altura': 1.63, 
+      'peso': 80
+    };
+  print(usuario.length);
+}
