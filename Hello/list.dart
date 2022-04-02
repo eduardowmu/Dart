2011344b5c00ca13dd4f221fce8417eb3e1d397c
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2022-04-01 23:54:18 -0300

    Mais sobre listas e maps

--- Hello/list.dart
+++ Hello/list.dart
@@ -1,4 +1,5 @@
 void main() {
   List numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Arroz", 1.56];
   print(numeros.length);
+  print(numeros[0]);
 }
--- Hello/listMap.dart
+++ Hello/listMap.dart
@@ -1,9 +1,17 @@
 void main() {
   Map usuario = {
-      'nome': 'Eduardo', 
-      'idade': 38, 
-      'altura': 1.63, 
-      'peso': 80
-    };
+    'nome': 'Eduardo', 
+    'idade': 38, 
+    'altura': 1.63, 
+    'peso': 80
+  };
   print(usuario.length);
+  print(usuario.entries);
+  print(usuario.values);
+  print(usuario.keys);
+  /*Perceba que para este caso o Dart não aceita*/
+  print(usuario[0]);
+  /*E para corrigir o caso anterior, devemos
+  especificar o respectivo dado*/
+  print(usuario["nome"]);
 }
